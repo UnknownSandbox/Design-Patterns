743430265dd19b46607a8254f8d9d9fdb1aaf395
UnknownHero <lord365567921@gmail.com>
2013-03-31 13:29:41 +0300

    Add pattern Bridge

---
+++ PatternsLib/Patterns/Bridge/Example/Abstraction.cs
@@ -0,0 +1,28 @@
+﻿namespace PatternsLib.Patterns.Bridge.Example
+{
+    /// <summary>
+    /// Abstraction - абстракция
+    /// </summary>
+    /// <remarks>
+    /// <li>
+    /// <lu>определяем интерфейс абстракции;</lu>
+    /// <lu>хранит ссылку на объект <see cref="Implementor"/></lu>
+    /// </li>
+    /// </remarks>
+    public class Abstraction
+    {
+        protected Implementor implementor;
+
+        // Property
+        public Implementor Implementor
+        {
+            get { return implementor; }
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+}
---
+++ PatternsLib/Patterns/Bridge/Example/ConcreteImplementorB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsLib.Patterns.Bridge.Example
+{
+    public class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB Operation");
+        }
+    }
+}
